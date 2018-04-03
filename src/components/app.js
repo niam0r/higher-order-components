a2ba2e7719b4a980d635fe7ec4d52b9d027b624f
Romain niam0r <niamor@mm.st>
2018-04-03 16:56:04 +0100

    Revert to f4056484aee5bd11815b13511406aaf79e38edcf

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
-import connect from 'react-redux';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div>React simple starter</div>
     );
   }
 }
-
-function mapStateToProps() {
-  return { posts: state.posts }
-}
-
-export default connect(mapStateToProps)(App);
