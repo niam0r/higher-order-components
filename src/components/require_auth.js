65a812b4de4e72885a36b1bce561fc054647bfa2
Romain niam0r <niamor@mm.st>
2018-04-03 19:18:34 +0100

    created ComposedComponent require_auth.js

---
+++ src/components/require_auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function(ComposedComponent) {
+  class Authentication extends Component {
+    render() {
+      return <ComposedComponent {...this.props} />
+    }
+  }
+
+  return Authentication;
+}
